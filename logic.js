0d7ed7d72dec12ac231ee4a9970c80d11cd28e4a
nishusingh30 <167319001+nishusingh30@users.noreply.github.com>
2024-04-18 18:39:35 +0530

    File Uploaded

---
+++ logic.js
@@ -0,0 +1,32 @@
+
+function page() {
+     window.location.href = 'product.html';
+ }
+ 
+ function page2() {
+    window.location.href = 'product2.html';
+}
+
+function page3() {
+    window.location.href = 'product3.html';
+}
+
+function page4() {
+    window.location.href = 'product4.html';
+}
+
+function page5() {
+    window.location.href = 'product5.html';
+}
+
+function page6() {
+    window.location.href = 'product6.html';
+}
+
+function page7() {
+    window.location.href = 'product7.html';
+}
+
+function page8() {
+    window.location.href = 'product8.html';
+}
